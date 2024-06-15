7c35cb3686de5f4f22598282bf7fa11349dde8d0
Hayden Young <22327045+hbjydev@users.noreply.github.com>
2024-06-15 23:48:12 +0100

    feat: switch to nixpkgs alloy package and service, some improvements to alloy ergonomics

--- modules/mixins/alloy/default.nix
+++ modules/mixins/alloy/default.nix
@@ -1,18 +1,15 @@
-{ pkgs-nix, pkgs, ... }: {
-  imports = [ pkgs-nix.nixosModules.alloy ];
-
+{ ... }: {
   environment.etc."alloy/config.alloy" = {
     source = ./config.alloy;
     mode = "0440";
     user = "root";
   };
 
-  services.alloy = {
-    enable = true;
-    package = pkgs-nix.packages.${pkgs.system}.alloy;
-    openFirewall = true;
-    configPath = "/etc/alloy";
-    group = "root";
+  environment.etc."alloy/base.alloy" = {
+    source = ./base.alloy;
+    mode = "0440";
     user = "root";
   };
+
+  services.alloy.enable = true;
 }
